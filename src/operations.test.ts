2d90dfcaa1d33468a9b433f6703b931c3a85cd8c
Vũ Đức Duy <duydvu98@gmail.com>
2023-04-30 13:21:14 +0700

    refactor: refactor operations test

--- src/operations.test.ts
+++ src/operations.test.ts
@@ -27,30 +27,28 @@ describe('Test parseExpression', () => {
     });
   });
 
-  test('INT op', () => {
-    expect(parseExpression('INT(a)', { a: '1' })).toBe(1);
-  });
+  describe('Type conversion ops', () => {
+    test('INT op', () => {
+      expect(parseExpression('INT(a)', { a: '1' })).toBe(1);
+    });
 
-  test('FLOAT op', () => {
-    expect(parseExpression('FLOAT(a)', { a: '1.234' })).toBe(1.234);
-  });
+    test('FLOAT op', () => {
+      expect(parseExpression('FLOAT(a)', { a: '1.234' })).toBe(1.234);
+    });
 
-  test('STRING op', () => {
-    expect(parseExpression('STRING(1)', {})).toBe('1');
-    expect(parseExpression('STRING(a)', { a: 123 })).toBe('123');
-  });
+    test('STRING op', () => {
+      expect(parseExpression('STRING(1)', {})).toBe('1');
+      expect(parseExpression('STRING(a)', { a: 123 })).toBe('123');
+    });
 
-  test('DATE op', () => {
-    expect(parseExpression('DATE(a)', { a: '2022-01-01' })).toEqual(new Date('2022-01-01'));
-    expect(parseExpression('DATE(a)', { a: 1640995200000 })).toEqual(new Date('2022-01-01'));
-  });
+    test('DATE op', () => {
+      expect(parseExpression('DATE(a)', { a: '2022-01-01' })).toEqual(new Date('2022-01-01'));
+      expect(parseExpression('DATE(a)', { a: 1640995200000 })).toEqual(new Date('2022-01-01'));
+    });
 
-  test('SLUG op', () => {
-    expect(parseExpression('SLUG(a)', { a: 'This is a title 123 !@#,./"' })).toBe('this-is-a-title-123-');
-  });
-
-  test('CURRENCY op', () => {
-    expect(parseExpression('CURRENCY(a)', { a: 1000 })).toBe('1,000');
+    test('CURRENCY op', () => {
+      expect(parseExpression('CURRENCY(a)', { a: 1000 })).toBe('1,000');
+    });
   });
 
   describe('Date ops', () => {
@@ -97,210 +95,217 @@ describe('Test parseExpression', () => {
     });
   });
 
-  test('ABS op', () => {
-    expect(parseExpression('ABS(a)', { a: -1 })).toBe(1);
+  describe('Arithmetic ops', () => {
+    test('ABS op', () => {
+      expect(parseExpression('ABS(a)', { a: -1 })).toBe(1);
+    });
+
+    test('SQRT op', () => {
+      expect(parseExpression('SQRT(a)', { a: 100 })).toBe(10);
+    });
+
+    test('SUM op', () => {
+      expect(parseExpression('SUM(a)', { a: [1, 2, 3, 4, 5] })).toBe(15);
+      expect(parseExpression('SUM(a)', { a: 1 })).toBe(0);
+    });
+
+    test('AVERAGE op', () => {
+      expect(parseExpression('AVERAGE(a)', { a: [1, 2, 3, 4, 5] })).toBe(3);
+      expect(parseExpression('AVERAGE(a)', { a: 1 })).toBe(0);
+    });
+
+    test('CEIL op', () => {
+      expect(parseExpression('CEIL(a)', { a: 1.234 })).toBe(2);
+    });
+
+    test('FLOOR op', () => {
+      expect(parseExpression('FLOOR(a)', { a: 1.234 })).toBe(1);
+    });
+
+    test('ROUND op', () => {
+      expect(parseExpression('ROUND(a)', { a: 1.234 })).toBe(1);
+      expect(parseExpression('ROUND(a, b)', { a: 5, b: 2 })).toBe('5.00');
+    });
+
+    test('EXP op', () => {
+      expect(parseExpression('EXP(a)', { a: 1 })).toBeCloseTo(Math.exp(1), 8);
+    });
+
+    test('LOG op', () => {
+      expect(parseExpression('LOG(a)', { a: 10 })).toBeCloseTo(Math.log(10), 8);
+    });
+
+    test('MAX op', () => {
+      expect(parseExpression('MAX(a)', { a: [1, 2, 30, 4, 5] })).toBe(30);
+      expect(parseExpression('MAX(a)', { a: 1 })).toBe(0);
+      expect(parseExpression('MAX(a, b)', { a: 5, b: 2 })).toBe(5);
+    });
+
+    test('MIN op', () => {
+      expect(parseExpression('MIN(a)', { a: [1, 2, 3, -4, 5] })).toBe(-4);
+      expect(parseExpression('MIN(a)', { a: 1 })).toBe(0);
+      expect(parseExpression('MIN(a, b)', { a: 5, b: 2 })).toBe(2);
+    });
+
+    test('SUM op', () => {
+      expect(parseExpression('SUM(a, b)', { a: 1, b: 2 })).toBe(3);
+    });
+
+    test('SUBTRACT op', () => {
+      expect(parseExpression('SUBTRACT(a, b)', { a: 5, b: 2 })).toBe(3);
+      expect(parseExpression('SUBTRACT(DATE(a), DATE(b))', { a: '2022-01-02', b: '2022-01-01' })).toBe(86400000);
+    });
+
+    test('MULTIPLY op', () => {
+      expect(parseExpression('MULTIPLY(a, b)', { a: 5, b: 2 })).toBe(10);
+    });
+
+    test('DIVIDE op', () => {
+      expect(parseExpression('DIVIDE(a, b)', { a: 5, b: 2 })).toBe(2.5);
+    });
+
+    test('REMAINDER op', () => {
+      expect(parseExpression('REMAINDER(a, b)', { a: 5, b: 2 })).toBe(1);
+    });
+
+    test('POWER op', () => {
+      expect(parseExpression('POWER(a, b)', { a: 5, b: 2 })).toBe(25);
+    });
   });
 
-  test('SQRT op', () => {
-    expect(parseExpression('SQRT(a)', { a: 100 })).toBe(10);
+  describe('Boolean ops', () => {
+    test('NULL op', () => {
+      expect(parseExpression('NULL(a)', { a: null })).toBe(true);
+      expect(parseExpression('NULL(a)', { a: undefined })).toBe(false);
+      expect(parseExpression('NULL(a)', { a: 0 })).toBe(false);
+      expect(parseExpression('NULL(a)', { a: '' })).toBe(false);
+      expect(parseExpression('NULL(a)', { a: {} })).toBe(false);
+      expect(parseExpression('NULL(a)', { a: [] })).toBe(false);
+    });
+
+    test('NOT_NULL op', () => {
+      expect(parseExpression('NOT_NULL(a)', { a: null })).toBe(false);
+      expect(parseExpression('NOT_NULL(a)', { a: undefined })).toBe(true);
+      expect(parseExpression('NOT_NULL(a)', { a: 0 })).toBe(true);
+      expect(parseExpression('NOT_NULL(a)', { a: '' })).toBe(true);
+      expect(parseExpression('NOT_NULL(a)', { a: {} })).toBe(true);
+      expect(parseExpression('NOT_NULL(a)', { a: [] })).toBe(true);
+    });
+
+    test('NOT op', () => {
+      expect(parseExpression('NOT(a)', { a: false })).toBe(true);
+      expect(parseExpression('NOT(a)', { a: true })).toBe(false);
+    });
+
+    test('EQUAL op', () => {
+      expect(parseExpression('EQUAL(a, b)', { a: 1, b: 1 })).toBe(true);
+      expect(parseExpression('EQUAL(a, b)', { a: 1, b: '1' })).toBe(false);
+    });
+
+    test('NOT_EQUAL op', () => {
+      expect(parseExpression('NOT_EQUAL(a, b)', { a: 1, b: 1 })).toBe(false);
+      expect(parseExpression('NOT_EQUAL(a, b)', { a: 1, b: '1' })).toBe(true);
+    });
+
+    test('GT op', () => {
+      expect(parseExpression('GT(a, b)', { a: 1, b: 2 })).toBe(false);
+      expect(parseExpression('GT(a, b)', { a: 1, b: 1 })).toBe(false);
+      expect(parseExpression('GT(a, b)', { a: 2, b: 1 })).toBe(true);
+    });
+
+    test('GTE op', () => {
+      expect(parseExpression('GTE(a, b)', { a: 1, b: 2 })).toBe(false);
+      expect(parseExpression('GTE(a, b)', { a: 1, b: 1 })).toBe(true);
+      expect(parseExpression('GTE(a, b)', { a: 2, b: 1 })).toBe(true);
+    });
+
+    test('LT op', () => {
+      expect(parseExpression('LT(a, b)', { a: 1, b: 2 })).toBe(true);
+      expect(parseExpression('LT(a, b)', { a: 1, b: 1 })).toBe(false);
+      expect(parseExpression('LT(a, b)', { a: 2, b: 1 })).toBe(false);
+    });
+
+    test('LTE op', () => {
+      expect(parseExpression('LTE(a, b)', { a: 1, b: 2 })).toBe(true);
+      expect(parseExpression('LTE(a, b)', { a: 1, b: 1 })).toBe(true);
+      expect(parseExpression('LTE(a, b)', { a: 2, b: 1 })).toBe(false);
+    });
+
+    test('AND op', () => {
+      expect(parseExpression('AND(a, b)', { a: true, b: true })).toBe(true);
+      expect(parseExpression('AND(a, b)', { a: true, b: false })).toBe(false);
+      expect(parseExpression('AND(a, b)', { a: false, b: true })).toBe(false);
+      expect(parseExpression('AND(a, b)', { a: false, b: false })).toBe(false);
+    });
+
+    test('OR op', () => {
+      expect(parseExpression('OR(a, b)', { a: true, b: true })).toBe(true);
+      expect(parseExpression('OR(a, b)', { a: true, b: false })).toBe(true);
+      expect(parseExpression('OR(a, b)', { a: false, b: true })).toBe(true);
+      expect(parseExpression('OR(a, b)', { a: false, b: false })).toBe(false);
+    });
   });
 
-  test('SUM op', () => {
-    expect(parseExpression('SUM(a)', { a: [1, 2, 3, 4, 5] })).toBe(15);
-    expect(parseExpression('SUM(a)', { a: 1 })).toBe(0);
+  describe('String ops', () => {
+    test('STR_LEN op', () => {
+      expect(parseExpression('STR_LEN(a)', { a: '123' })).toBe(3);
+      expect(parseExpression('STR_LEN(a)', { a: 1 })).toBe(1);
+    });
+
+    test('LOWER op', () => {
+      expect(parseExpression('LOWER(a)', { a: 'ABCDEF' })).toBe('abcdef');
+    });
+
+    test('UPPER op', () => {
+      expect(parseExpression('UPPER(a)', { a: 'abcdef' })).toBe('ABCDEF');
+    });
+
+    test('TRIM op', () => {
+      expect(parseExpression('TRIM(a)', { a: '   abc  def   ' })).toBe('abc  def');
+    });
+
+    test('CONCAT op', () => {
+      expect(parseExpression('CONCAT(a, b)', { a: '123', b: '456' })).toBe('123456');
+    });
+
+    test('LEFT op', () => {
+      expect(parseExpression('LEFT(a, b)', { a: '123456', b: 2 })).toBe('12');
+    });
+
+    test('RIGHT op', () => {
+      expect(parseExpression('RIGHT(a, b)', { a: '123456', b: 2 })).toBe('56');
+    });
+
+    test('SLUG op', () => {
+      expect(parseExpression('SLUG(a)', { a: 'This is a title 123 !@#,./"' })).toBe('this-is-a-title-123-');
+    });
   });
 
-  test('AVERAGE op', () => {
-    expect(parseExpression('AVERAGE(a)', { a: [1, 2, 3, 4, 5] })).toBe(3);
-    expect(parseExpression('AVERAGE(a)', { a: 1 })).toBe(0);
+  describe('Array ops', () => {
+    test('ARRAY_LEN op', () => {
+      expect(parseExpression('ARRAY_LEN(a)', { a: [1, 2, 3] })).toBe(3);
+      expect(parseExpression('ARRAY_LEN(a)', { a: 1 })).toBe(0);
+    });
   });
 
-  test('CEIL op', () => {
-    expect(parseExpression('CEIL(a)', { a: 1.234 })).toBe(2);
+  describe('Relational ops', () => {
+    test('ASUM op', () => {
+      expect(parseExpression('ASUM(a, b)', { a: [{b: 5}, {b: 10}, {b: 0}, {b: 15}] })).toBe(30);
+      expect(parseExpression('ASUM(a, MULTIPLY(b, c))', { a: [{b: 5, c: 1}, {b: 10, c: 2}, {b: 1000, c: 0}, {b: 15, c: 10}] })).toBe(175);
+    });
   });
 
-  test('FLOOR op', () => {
-    expect(parseExpression('FLOOR(a)', { a: 1.234 })).toBe(1);
-  });
-
-  test('ROUND op', () => {
-    expect(parseExpression('ROUND(a)', { a: 1.234 })).toBe(1);
-  });
-
-  test('EXP op', () => {
-    expect(parseExpression('EXP(a)', { a: 1 })).toBeCloseTo(Math.exp(1), 8);
-  });
-
-  test('LOG op', () => {
-    expect(parseExpression('LOG(a)', { a: 10 })).toBeCloseTo(Math.log(10), 8);
-  });
-
-  test('MAX op', () => {
-    expect(parseExpression('MAX(a)', { a: [1, 2, 30, 4, 5] })).toBe(30);
-    expect(parseExpression('MAX(a)', { a: 1 })).toBe(0);
-  });
-
-  test('MIN op', () => {
-    expect(parseExpression('MIN(a)', { a: [1, 2, 3, -4, 5] })).toBe(-4);
-    expect(parseExpression('MIN(a)', { a: 1 })).toBe(0);
-  });
-
-  test('NULL op', () => {
-    expect(parseExpression('NULL(a)', { a: null })).toBe(true);
-    expect(parseExpression('NULL(a)', { a: undefined })).toBe(false);
-    expect(parseExpression('NULL(a)', { a: 0 })).toBe(false);
-    expect(parseExpression('NULL(a)', { a: '' })).toBe(false);
-    expect(parseExpression('NULL(a)', { a: {} })).toBe(false);
-    expect(parseExpression('NULL(a)', { a: [] })).toBe(false);
-  });
-
-  test('NOT_NULL op', () => {
-    expect(parseExpression('NOT_NULL(a)', { a: null })).toBe(false);
-    expect(parseExpression('NOT_NULL(a)', { a: undefined })).toBe(true);
-    expect(parseExpression('NOT_NULL(a)', { a: 0 })).toBe(true);
-    expect(parseExpression('NOT_NULL(a)', { a: '' })).toBe(true);
-    expect(parseExpression('NOT_NULL(a)', { a: {} })).toBe(true);
-    expect(parseExpression('NOT_NULL(a)', { a: [] })).toBe(true);
-  });
-
-  test('NOT op', () => {
-    expect(parseExpression('NOT(a)', { a: false })).toBe(true);
-    expect(parseExpression('NOT(a)', { a: true })).toBe(false);
-  });
-
-  test('STR_LEN op', () => {
-    expect(parseExpression('STR_LEN(a)', { a: '123' })).toBe(3);
-    expect(parseExpression('STR_LEN(a)', { a: 1 })).toBe(1);
-  });
-
-  test('LOWER op', () => {
-    expect(parseExpression('LOWER(a)', { a: 'ABCDEF' })).toBe('abcdef');
-  });
-
-  test('UPPER op', () => {
-    expect(parseExpression('UPPER(a)', { a: 'abcdef' })).toBe('ABCDEF');
-  });
-
-  test('TRIM op', () => {
-    expect(parseExpression('TRIM(a)', { a: '   abc  def   ' })).toBe('abc  def');
-  });
-
-  test('ARRAY_LEN op', () => {
-    expect(parseExpression('ARRAY_LEN(a)', { a: [1, 2, 3] })).toBe(3);
-    expect(parseExpression('ARRAY_LEN(a)', { a: 1 })).toBe(0);
-  });
-
-  test('SUM op', () => {
-    expect(parseExpression('SUM(a, b)', { a: 1, b: 2 })).toBe(3);
-  });
-
-  test('SUBTRACT op', () => {
-    expect(parseExpression('SUBTRACT(a, b)', { a: 5, b: 2 })).toBe(3);
-    expect(parseExpression('SUBTRACT(DATE(a), DATE(b))', { a: '2022-01-02', b: '2022-01-01' })).toBe(86400000);
-  });
-
-  test('MULTIPLY op', () => {
-    expect(parseExpression('MULTIPLY(a, b)', { a: 5, b: 2 })).toBe(10);
-  });
-
-  test('DIVIDE op', () => {
-    expect(parseExpression('DIVIDE(a, b)', { a: 5, b: 2 })).toBe(2.5);
-  });
-
-  test('REMAINDER op', () => {
-    expect(parseExpression('REMAINDER(a, b)', { a: 5, b: 2 })).toBe(1);
-  });
-
-  test('ROUND op', () => {
-    expect(parseExpression('ROUND(a, b)', { a: 5, b: 2 })).toBe('5.00');
-  });
-
-  test('MAX op', () => {
-    expect(parseExpression('MAX(a, b)', { a: 5, b: 2 })).toBe(5);
-  });
-
-  test('MIN op', () => {
-    expect(parseExpression('MIN(a, b)', { a: 5, b: 2 })).toBe(2);
-  });
-
-  test('POWER op', () => {
-    expect(parseExpression('POWER(a, b)', { a: 5, b: 2 })).toBe(25);
-  });
-
-  test('CONCAT op', () => {
-    expect(parseExpression('CONCAT(a, b)', { a: '123', b: '456' })).toBe('123456');
-  });
-
-  test('LEFT op', () => {
-    expect(parseExpression('LEFT(a, b)', { a: '123456', b: 2 })).toBe('12');
-  });
-
-  test('RIGHT op', () => {
-    expect(parseExpression('RIGHT(a, b)', { a: '123456', b: 2 })).toBe('56');
-  });
-
-  test('EQUAL op', () => {
-    expect(parseExpression('EQUAL(a, b)', { a: 1, b: 1 })).toBe(true);
-    expect(parseExpression('EQUAL(a, b)', { a: 1, b: '1' })).toBe(false);
-  });
-
-  test('NOT_EQUAL op', () => {
-    expect(parseExpression('NOT_EQUAL(a, b)', { a: 1, b: 1 })).toBe(false);
-    expect(parseExpression('NOT_EQUAL(a, b)', { a: 1, b: '1' })).toBe(true);
-  });
-
-  test('GT op', () => {
-    expect(parseExpression('GT(a, b)', { a: 1, b: 2 })).toBe(false);
-    expect(parseExpression('GT(a, b)', { a: 1, b: 1 })).toBe(false);
-    expect(parseExpression('GT(a, b)', { a: 2, b: 1 })).toBe(true);
-  });
-
-  test('GTE op', () => {
-    expect(parseExpression('GTE(a, b)', { a: 1, b: 2 })).toBe(false);
-    expect(parseExpression('GTE(a, b)', { a: 1, b: 1 })).toBe(true);
-    expect(parseExpression('GTE(a, b)', { a: 2, b: 1 })).toBe(true);
-  });
-
-  test('LT op', () => {
-    expect(parseExpression('LT(a, b)', { a: 1, b: 2 })).toBe(true);
-    expect(parseExpression('LT(a, b)', { a: 1, b: 1 })).toBe(false);
-    expect(parseExpression('LT(a, b)', { a: 2, b: 1 })).toBe(false);
-  });
-
-  test('LTE op', () => {
-    expect(parseExpression('LTE(a, b)', { a: 1, b: 2 })).toBe(true);
-    expect(parseExpression('LTE(a, b)', { a: 1, b: 1 })).toBe(true);
-    expect(parseExpression('LTE(a, b)', { a: 2, b: 1 })).toBe(false);
-  });
-
-  test('AND op', () => {
-    expect(parseExpression('AND(a, b)', { a: true, b: true })).toBe(true);
-    expect(parseExpression('AND(a, b)', { a: true, b: false })).toBe(false);
-    expect(parseExpression('AND(a, b)', { a: false, b: true })).toBe(false);
-    expect(parseExpression('AND(a, b)', { a: false, b: false })).toBe(false);
-  });
-
-  test('OR op', () => {
-    expect(parseExpression('OR(a, b)', { a: true, b: true })).toBe(true);
-    expect(parseExpression('OR(a, b)', { a: true, b: false })).toBe(true);
-    expect(parseExpression('OR(a, b)', { a: false, b: true })).toBe(true);
-    expect(parseExpression('OR(a, b)', { a: false, b: false })).toBe(false);
-  });
-
-  test('ASUM op', () => {
-    expect(parseExpression('ASUM(a, b)', { a: [{b: 5}, {b: 10}, {b: 0}, {b: 15}] })).toBe(30);
-    expect(parseExpression('ASUM(a, MULTIPLY(b, c))', { a: [{b: 5, c: 1}, {b: 10, c: 2}, {b: 1000, c: 0}, {b: 15, c: 10}] })).toBe(175);
-  });
-
-  test('IF op', () => {
-    expect(parseExpression('IF(a, b, c)', { a: true, b: 1, c: 2})).toBe(1);
-    expect(parseExpression('IF(a, b, c)', { a: false, b: 1, c: 2})).toBe(2);
-    expect(parseExpression('IF(a, b, c)', { a: 1, b: 1, c: 2})).toBe(2);
-    expect(parseExpression('IF(a, b, c)', { a: '1', b: 1, c: 2})).toBe(2);
-    expect(parseExpression('IF(a, b, c)', { a: {}, b: 1, c: 2})).toBe(2);
-    expect(parseExpression('IF(a, b, c)', { a: [], b: 1, c: 2})).toBe(2);
-    expect(parseExpression('IF(EQUAL(a, 5), b, c)', { a: 5, b: 1, c: 2})).toBe(1);
-    expect(parseExpression('IF(AND(GT(a, 0), LT(a, 10)), b, c)', { a: 5, b: 1, c: 2})).toBe(1);
+  describe('Condition ops', () => {
+    test('IF op', () => {
+      expect(parseExpression('IF(a, b, c)', { a: true, b: 1, c: 2})).toBe(1);
+      expect(parseExpression('IF(a, b, c)', { a: false, b: 1, c: 2})).toBe(2);
+      expect(parseExpression('IF(a, b, c)', { a: 1, b: 1, c: 2})).toBe(2);
+      expect(parseExpression('IF(a, b, c)', { a: '1', b: 1, c: 2})).toBe(2);
+      expect(parseExpression('IF(a, b, c)', { a: {}, b: 1, c: 2})).toBe(2);
+      expect(parseExpression('IF(a, b, c)', { a: [], b: 1, c: 2})).toBe(2);
+      expect(parseExpression('IF(EQUAL(a, 5), b, c)', { a: 5, b: 1, c: 2})).toBe(1);
+      expect(parseExpression('IF(AND(GT(a, 0), LT(a, 10)), b, c)', { a: 5, b: 1, c: 2})).toBe(1);
+    });
   });
 
   describe('Nested expressions', () => {
